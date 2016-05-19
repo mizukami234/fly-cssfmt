ab5d59b861ac0e3a6913c7f0e66c0315e27867bb
Shun MIZUKAMI <norainu234+dev@gmail.com>
2016-05-19 11:49:28 +0900

    support fly v1.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export default function () {
-  this.filter("cssfmt", (data) => {
+module.exports = function () {
+  this.filter("cssfmt", function(data) {
     try {
       return require("cssfmt").process(data.toString())
      } catch (e) { throw e }
